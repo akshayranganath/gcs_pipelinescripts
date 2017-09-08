0a9956759c6e0b8bbd944e3d56594a190cbd6eaf
Akshay Ranganath <akrangan@akamai.com>
2017-09-08 14:46:31 -0700

    Adding a new merge checking script

---
+++ check_for_merge_closure.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import logging
+
+FORMAT = '%(asctime)-15s %(levelname)s: %(message)s'
+logging.basicConfig(format=FORMAT)
+#logging.basicConfig()
+logger = logging.getLogger('filecleaner')
+logger.setLevel(logging.DEBUG)
+
+merge_request = False
+raw_data = sys.stdin.read()
+payload = json.loads(raw_data)
+logger.info('Loaded the payload data successfully')
+if 'action' in payload and payload['action']=='closed':
+	logger.debug('Found a close action')
+	if 'pull_request' in payload and 'merged_at' in payload['pull_request']:
+		logger.debug('Found a merged_at within a pull_request')
+		merge_request = True
+try:
+	assert merge_request==True
+	logger.info('Found a successful merge request.')
+except AssertionError:
+	logger.info('Merge request was not found.')
+	raise AssertionError
